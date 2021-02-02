371b10eb5925cff08feb544118987b622220ab79
Rajeshwari <rvariar10@gmail.com>
2021-02-01 17:12:38 -0800

    Initialize and register the routes.

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const routes = require('./routes/rewardPointsRoute'); //importing route
+
+// initialize express & bodyParser.
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: true }));
+port = process.env.PORT || 3000;
+// Register routes.
+routes(app);
+// Listen on port.
+app.listen(port);
+console.log('RESTful API server started on: ' + port);
